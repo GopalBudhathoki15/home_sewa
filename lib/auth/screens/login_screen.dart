787480e90ecd04b35ac60d0f75cccb5c655fe180
GopalBudhathoki15 <gopal.budhathoki8@gmail.com>
2023-06-13 15:18:41 +0545

    login page ui complete

---
+++ lib/auth/screens/login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../core/common/sign_in_button.dart';
+import '../../core/constants/constants.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Home Sewa",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.asset(
+              Constants.homeservice,
+              height: 400,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Bringing Home Services to Your Doorstep!',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SignInButton(),
+        ],
+      ),
+    );
+  }
+}
